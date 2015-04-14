6169fe3fb3e81d74f86add4041b4c3f90438623d
softvision-dancostin <dan.costin@softvision.ro>
2015-04-14 17:59:25 +0300

    Added Chapter 19

---
+++ AsynchronousDelegatesExample/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousDelegatesExample
+{
+    public delegate int BinaryOp(int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Primary Thread stats *****");
+            Thread.CurrentThread.Name = "ThePrimaryThread";
+            ShowThreadStats();
+
+            Console.WriteLine("***** Async Delegate Invocation *****");
+            Console.WriteLine("Main() invoked on thread {0}.",
+            Thread.CurrentThread.ManagedThreadId);
+            BinaryOp binaryOp = new BinaryOp(Add);
+            IAsyncResult iftAR = binaryOp.BeginInvoke(10, 10, null, null);
+            while (!iftAR.IsCompleted)
+            {
+                Console.WriteLine("Doing more work in Main()!");
+                Thread.Sleep(1000);
+            }
+            int answer = binaryOp.EndInvoke(iftAR);
+            Console.WriteLine("10 + 10 is {0}.", answer);
+            Console.ReadLine();
+        }
+
+        static int Add(int x, int y)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("***** Secondary Thread stats *****");
+            Thread.CurrentThread.Name = "TheSecondaryThread";
+            ShowThreadStats();
+
+            Console.WriteLine("Add() invoked on thread {0}.",
+            Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(5000);
+            return x + y;
+        }
+
+        static void ShowThreadStats()
+        {
+            // Obtain and name the current thread.
+            Thread primaryThread = Thread.CurrentThread;
+            // Show details of hosting AppDomain/Context.
+            Console.WriteLine("Name of current AppDomain: {0}",
+            Thread.GetDomain().FriendlyName);
+            Console.WriteLine("ID of current Context: {0}",
+            Thread.CurrentContext.ContextID);
+            // Print out some stats about this thread.
+            Console.WriteLine("Thread Name: {0}",
+            primaryThread.Name);
+            Console.WriteLine("Has thread started?: {0}",
+            primaryThread.IsAlive);
+            Console.WriteLine("Priority Level: {0}",
+            primaryThread.Priority);
+            Console.WriteLine("Thread State: {0}",
+            primaryThread.ThreadState);
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ ConcurencyExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurencyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Synchronizing Threads *****\n");
+            Printer p = new Printer();
+            // Make 10 threads that are all pointing to the same
+            // method on the same object.
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] =
+                new Thread(new ThreadStart(p.PrintNumbers));
+                threads[i].Name = string.Format("Worker thread #{0}", i);
+            }
+            // Now start each one.
+            foreach (Thread t in threads)
+                t.Start();
+            Console.ReadLine();
+        }
+    }
+}
