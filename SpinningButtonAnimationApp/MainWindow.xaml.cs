00ace6552d6fb20917e129d817a68aebff242af3
softvision-dancostin <dan.costin@softvision.ro>
2015-04-16 18:22:12 +0300

    Complete Chapter 30

---
+++ SpinningButtonAnimationApp/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpinningButtonAnimationApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool isSpinning = false;
+
+        private void btnSpinner_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (!isSpinning)
+            {
+                isSpinning = true;
+                DoubleAnimation dblAnim = new DoubleAnimation();
+                dblAnim.Completed += (o, s) => { isSpinning = false; };
+                dblAnim.From = 0;
+                dblAnim.To = 360;
+                dblAnim.Duration = new Duration(TimeSpan.FromSeconds(4));
+                dblAnim.AutoReverse = true;
+                RotateTransform rt = new RotateTransform();
+                btnSpinner.RenderTransform = rt;
+                rt.BeginAnimation(RotateTransform.AngleProperty, dblAnim);
+            }
+        }
+
+        private void btnSpinner_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation dblAnim = new DoubleAnimation();
+            dblAnim.From = 1.0;
+            dblAnim.To = 0.0;
+            dblAnim.AutoReverse = true;
+            btnSpinner.BeginAnimation(Button.OpacityProperty, dblAnim);
+        }
+    }
+}
---
+++ TreesAndTemplatesApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace TreesAndTemplatesApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string dataToShow;
+
+        private void btnShowLogicalTree_Click(object sender, RoutedEventArgs e)
+        {
+            dataToShow = "";
+            BuildLogicalTree(0, this);
+            this.txtDisplayArea.Text = dataToShow;
+        }
+
+        private void btnShowVisualTree_Click(object sender, RoutedEventArgs e)
+        {
+            dataToShow = "";
+            BuildVisualTree(0, this);
+            this.txtDisplayArea.Text = dataToShow;
+        }
+
+        void BuildLogicalTree(int depth, object obj)
+        {
+            dataToShow += new string(' ', depth) + obj.GetType().Name + "\n";
+            if (!(obj is DependencyObject))
+                return;
+            foreach (object child in LogicalTreeHelper.GetChildren(
+            obj as DependencyObject))
+                BuildLogicalTree(depth + 5, child);
+        }
+
+        void BuildVisualTree(int depth, DependencyObject obj)
+        {
+            dataToShow += new string(' ', depth) + obj.GetType().Name + "\n";
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+                BuildVisualTree(depth + 1, VisualTreeHelper.GetChild(obj, i));
+        }
+    }
+}
